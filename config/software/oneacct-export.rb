f2a16e82844a29b8d48ed1aac0f03b516ffa96fb
Boris Parak <parak@cesnet.cz>
2016-01-06 18:17:29 +0100

    Fixing broken deb* builds
    Missing ENV was causing cross-linking between embedded versions of
    libraries and system libraries.

--- config/software/oneacct-export.rb
+++ config/software/oneacct-export.rb
@@ -6,6 +6,17 @@ dependency "ruby"
 dependency "rubygems"
 dependency "liblzma"
 
+env = {
+  "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+  "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+  "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
+}
+
 build do
-  gem "install oneacct-export -n #{install_dir}/bin --no-rdoc --no-ri -v #{version}"
+  gem "install oneacct-export -n #{install_dir}/bin --no-rdoc --no-ri -v #{version}", :env => env
+  delete "#{install_dir}/embedded/docs"
+  delete "#{install_dir}/embedded/share/man"
+  delete "#{install_dir}/embedded/share/doc"
+  delete "#{install_dir}/embedded/ssl/man"
+  delete "#{install_dir}/embedded/info"
 end
